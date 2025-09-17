e833c3f3a98f246418ea39cf2d116b76f13bb7ec
Edwiin22 <20243rd034@utez.edu.mx>
2025-09-17 16:06:49 -0600

    Agregue un repos

--- Git_scaner.py
+++ Git_scaner.py
@@ -9,3 +9,10 @@ git_dir = r"C:\Users\CC6\Documents\Ejemplo"
 repos = [name for name in os.listdir(git_dir)
          if os.path.isdir(os.path.join(git_dir, name)) and 
          os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+if repos:
+    print("found the following GitHub Repositorios: ")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx}. {repo}")
+
+   
